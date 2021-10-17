9b1c9c3a06ba9d3c52b3d6df008ee6834c3f1447
pencer118 <pencer118@gmail.com>
2021-10-17 21:49:21 +0900

    1017 mvc 패턴과 템플릿엔진(thymeleaf),api연습

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,4 +20,31 @@ public class HelloController {
         model.addAttribute("name",name);
         return "hello-template";
     }
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name; //"hello spring!!"
+    }
+    // @ ResponseBody 와 @RequestParam 에 대해 더 알아보기
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;  //json 형식으로 내려준다.
+    }
+
+
+        static class Hello {
+        private String name;
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+        }
+
 }
